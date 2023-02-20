c51e911e26dec3f7e1b508c70298e16e10483175
Respect6 <112162659+Respect6@users.noreply.github.com>
2023-02-20 21:56:16 +0100

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,33 +1,26 @@
 
-function mouseEnter() {
-    document.getElementById("card").style.color = "red";
-};
-
-function makeEaseOut(timing) {
-    return function (timeFraction) {
-        return 1 - timing(1 - timeFraction);
-    }
-}
-
-function bounce(timeFraction) {
-    for (let a = 0, b = 1; 1; a += b, b /= 2) {
-        if (timeFraction >= (7 - 4 * a) / 11) {
-            return -Math.pow((11 - 6 * a - 11 * timeFraction) / 4, 2) + Math.pow(b, 2)
-        }
-    }
-}
-
-ball.onclick = function () {
-
-    let to = field.clientHeight - ball.clientHeight;
-
-    animate({
-        duration: 2000,
-        timing: makeEaseOut(bounce),
-        draw(progress) {
-            ball.style.top = to * progress + 'px'
-        }
-    });
+// var dudes = document.querySelectorAll('.choice div');
+// dudes = Array.prototype.slice.call(dudes); //dudes = Array.from( dudes );
 
 
-};
+// dudes.forEach(function (dude) {
+
+//     dude.addEventListener('click', function () {
+
+//         var scoreElement = this.nextElementSibling,
+//             score = Number(scoreElement.textContent);
+
+//         score = score + 1;
+//         scoreElement.textContent = score;
+//     });
+
+
+//     dude.addEventListener('mouseover', function () {
+//         this.classList.add('desaturate');
+//     });
+
+//     dude.addEventListener('mouseout', function () {
+//         this.classList.remove('desaturate');
+//     });
+
+// });
